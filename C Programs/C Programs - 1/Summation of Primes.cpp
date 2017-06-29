293ddc98a7f7231e7d7dbc71341a7f9b763544e9
Saikat Ghosh <ghosh_saikat4000@yahoo.co.in>
2017-06-29 10:49:54 +0530

    Create Summation of Primes.cpp

---
+++ C Programs/C Programs - 1/Summation of Primes.cpp
@@ -0,0 +1,41 @@
+#include <cstdio>
+#include <vector>
+
+using namespace std;
+
+vector <long long> prime_sum_till(1e6, 0);
+
+void precompute_primes()
+{
+    vector <int> is_prime(1e6 + 1, true);
+    is_prime[0] = is_prime[1] = false;
+
+    for(int i = 2; i <= 1e6; i++)
+    {
+        if(is_prime[i])
+        {
+            for(int multiple = 2*i; multiple <= 1e6; multiple +=i)
+            {
+                is_prime[multiple] = false;
+            }
+        }
+    }
+
+    for(int i = 2; i <= 1e6; i++)
+    {
+        prime_sum_till[i] = prime_sum_till[i - 1] + (is_prime[i] ? i : 0);
+    }
+}
+
+int main()
+{
+    precompute_primes();
+
+    int n;
+    scanf("%d", &n);
+    printf("%I64d\n", prime_sum_till[n]);
+
+    return 0;
+}
+
+
